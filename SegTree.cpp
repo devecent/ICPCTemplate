e97b5f08b866722be8ec7ebf8dd0bd3a1821614c
devecent <138352025+devecent@users.noreply.github.com>
2025-11-05 00:32:15 -0600

    Update SegTree.cpp

--- SegTree.cpp
+++ SegTree.cpp
@@ -1,43 +1,50 @@
-long long merge(long long a, long long b) {
-	return a+b;
-}
-
-struct SegTree {
-	int n;
-	vector<long long> tree;
-
-	SegTree(vector<int>& a) {
-		n = a.size();
-		tree = vector<long long>(2*n);
-		for(int i = n; i < 2*n; i++) {
-			tree[i] = a[i-n]; 
-		}
-		for(int i = n-1; i >= 1; i--) {
-			tree[i] = merge(tree[i*2],tree[i*2+1]);
-		}
-	}
-
-	SegTree(int n) {
-		tree = vector<long long>(2*n);
-	}
-
-	long long query(int l, int r) {
-		l += n; r += n;
-		long long left = 0, right = 0;
-		while(l <= r) {
-			if(l % 2 == 1) left = merge(left,tree[l++]);
-			if(r % 2 == 0) right = merge(tree[r--],right);
-			l >>= 1;
-			r >>= 1;
-		}
-		return merge(left,right);
-	}
-
-	void update(int i, long long val) {
-		i += n;
-		tree[i] = val;
-		for(i >>= 1; i >= 1; i >>= 1) {
-			tree[i] = merge(tree[i*2],tree[i*2+1]);
-		}
-	}
+template<class B> struct SegTree : public B {
+    using T = typename B::T;
+ 
+    size_t n; vector<T> tree;
+ 
+    SegTree(vector<T>& data) {
+        n = 1 << (32 - __builtin_clz(data.size()));
+        tree = vector<T>(n * 2, B::base);
+        buildTree(data);
+    }
+ 
+    SegTree(size_t sz) {
+        n = 1 << (32 - __builtin_clz(sz));
+        tree = vector<T>(n * 2, B::base);
+    }
+ 
+    T query(int l, int r) {
+        l += n; r += n;
+        T left = B::base, right = B::base;
+        while (l <= r) {
+            if (l % 2 == 1) left = B::merge(left, tree[l++]);
+            if (r % 2 == 0) right = B::merge(tree[r--], right);
+            l >>= 1; r >>= 1;
+        }
+        return B::merge(left, right);
+    }
+ 
+    T get(int i) {
+        return tree[i + n];
+    }
+ 
+    void update(int i, T val) {
+        i += n; tree[i] = val;
+        for (i >>= 1; i >= 1; i >>= 1)
+            tree[i] = B::merge(tree[2*i], tree[2*i+1]);
+    }
+ 
+    void buildTree(vector<T>& data) {
+        for (int i = n; i < n + data.size(); i++)
+            tree[i] = data[i-n];
+        for (int i = n-1; i >= 1; i--)
+            tree[i] = B::merge(tree[2*i], tree[2*i+1]);
+    }
+};
+ 
+struct AddInt {
+	using T = int;
+	const T base = 0;
+	T merge(T a, T b) { return a + b; }
 };
